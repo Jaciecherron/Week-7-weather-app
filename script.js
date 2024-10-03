db6bebea113366ce0c98f85aa90e2635850e23ae
Jaciecherron <jaciecherron@gmail.com>
2024-10-03 11:50:10 -0600

    Added hour and minute adjustments

--- script.js
+++ script.js
@@ -13,9 +13,15 @@ let now = new Date ();
 let date = document.querySelector ("#dateTime");
 let currenteDate = now.getDate();
 let hour = now.getHours();
+if (hour <10){
+  hour = `0${hour}`;
+}
 let minutes = now.getMinutes();
+if (minutes <10) {
+  minutes = `0${minutes}`;
+}
 let day= days[now.getDay()];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let month = months [now.getMonth()];
 let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-date.innherHTML = `${day} ${month} ${day}, ${hour}:${minutes}`;
+date.innerHTML = `${day} ${month} ${day}, ${hour}:${minutes}`;
