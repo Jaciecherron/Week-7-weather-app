85b56f930e4f22e46467a7ca9b9c42570295c4f9
Jaciecherron <jaciecherron@gmail.com>
2024-10-03 11:39:09 -0600

    Working on date and time

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function searchSubmit (event) {
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", searchSubmit)
+
+let now = new Date ();
+let date = document.querySelector ("#dateTime");
+let currenteDate = now.getDate();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let day= days[now.getDay()];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let month = months [now.getMonth()];
+let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+date.innherHTML = `${day} ${month} ${day}, ${hour}:${minutes}`;
